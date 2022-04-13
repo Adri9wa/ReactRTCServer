9dfdd4f30e902ed079b3122ae5982de3781fa0e4
GT <FTowerT@outlook.com>
2022-04-13 23:54:58 +0300

    Added new fields to devices page

--- src/Pages/Devices/index.js
+++ src/Pages/Devices/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {connect} from "react-redux";
 import {fetchDevices, selectDevices} from "./redux/duck";
+import Styles from './styles.module.css';
 
 const mapStateToProps = state => ({
     devices: selectDevices(state),
@@ -16,11 +17,22 @@ class Devices extends Component {
         this.props.fetchDevices();
     }
     
+    renderDevice(device) {
+        return <div className={Styles.deviceContainer} key={device.id}>
+            <span className={Styles.deviceID}>ID: {device.id}</span>
+            <span className={Styles.deviceCode}>Code: {device.deviceCode}</span>
+            <span>{device.name}</span>
+        </div>
+    }
+    
     render() {
         console.log('devices: ', this.props.devices)
         return (
-            <div>
-                {JSON.stringify(this.props.devices)}
+            <div className={Styles.container}>
+                {this.props.devices.map(device => {
+                    return this.renderDevice(device);
+                })}
+                {/*{JSON.stringify(this.props.devices)}*/}
             </div>
         )
     }
