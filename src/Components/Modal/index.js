505ddca306c2460a8b4797e08d7007728efced7f
GT <FTowerT@outlook.com>
2022-04-15 21:41:34 +0300

    Modal styles improved

--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
-import ReactDOM from "react-dom";
-import { CSSTransition } from "react-transition-group";
 import Styles from "./styles.module.css";
+import {Button} from "rtc-ui-library";
 
 /**
  * Added this modal, but it is not finished, and it doesn't work completely
@@ -9,29 +8,9 @@ import Styles from "./styles.module.css";
  * @param props.visible
  * @param props.title
  * @param props.children - children node
- * @return {}
  * @constructor
  */
 export class Modal extends React.Component {
-    // const closeOnEscapeKeyDown = e => {
-    //     if ((e.charCode || e.keyCode) === 27) {
-    //         props.onClose();
-    //     }
-    // };
-    //
-    // useEffect(() => {
-    //     document.body.addEventListener("keydown", closeOnEscapeKeyDown);
-    //     return function cleanup() {
-    //         document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
-    //     };
-    //
-    //     setTimeout(() => {
-    //         console.log('Auto close')
-    //         if(props.onClose) props.onClose();
-    //     }, 2000)
-    // }, []);
-    //
-    
     render() {
         return <div className={this.props.visible? Styles.modal: Styles.modalInvisible} onClick={this.props.onClose}>
             <div className={Styles.modalContent} onClick={e => e.stopPropagation()}>
@@ -40,9 +19,9 @@ export class Modal extends React.Component {
                 </div>
                 <div className={Styles.modalBody}>{this.props.children}</div>
                 <div className={Styles.modalFooter}>
-                    <button onClick={this.props.onClose}>
+                    <Button onClick={this.props.onClose}>
                         Close
-                    </button>
+                    </Button>
                 </div>
             </div>
         </div>;
