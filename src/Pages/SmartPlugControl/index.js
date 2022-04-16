d67bf07a6ea9eb56690db9ebf003d9af68cea1ab
GT <FTowerT@outlook.com>
2022-04-16 12:59:39 +0300

    Added Ability to create, edit and delete smart plug parameters

--- src/Pages/SmartPlugControl/index.js
+++ src/Pages/SmartPlugControl/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Styles from './styles.module.css'
-import { TitleText, Text, Switcher } from 'rtc-ui-library';
+import {TitleText, Text, Switcher, Button} from 'rtc-ui-library';
 import { withRouter } from "react-router";
 import {selectSmartPlug, fetchSmartPlug, selectParameters, selectParametersStats} from './redux/duck';
 import {connect} from "react-redux";
@@ -8,6 +8,8 @@ import {connect} from "react-redux";
 import { fetchAPI } from 'utils';
 import Block from "../../Components/Block";
 import SpanBlock from "../../Components/SpanBlock";
+import Modal from "../../Components/Modal";
+import DeviceParameterModal from "../../Modals/DeviceParameterModal/DeviceParameterModal";
 
 export const COLORS = {
     success: '#28C438',
@@ -33,6 +35,8 @@ class SmartPlugControl extends Component{
 
         this.state = {
             onOff: false, //On Off value of the lamp
+            visible: false,
+            currentDeviceID: this.props.match.params.id
         }
     }
     
@@ -72,16 +76,47 @@ class SmartPlugControl extends Component{
     }
     
     renderParameter = (parameter) => {
-        return <Block key={parameter.id}>
-            <SpanBlock className={Styles.parameterID}>{parameter.id}</SpanBlock>
-            <SpanBlock className={Styles.parameterKey}>{parameter.key}</SpanBlock>
-            {
-                parameter.value
-                    ? <SpanBlock className={Styles.parameterValue}>{parameter.value}</SpanBlock>
-                    : "NO_VALUE"
-            }
-            <SpanBlock className={Styles.parameterType}>{parameter.type}</SpanBlock>
-        </Block>
+        return <div key={parameter.id} className={Styles.parameterContainer}>
+            <div className={Styles.parameterDataContainer}>
+                <Block>
+                    <SpanBlock className={Styles.parameterID}>{parameter.id}</SpanBlock>
+                    <SpanBlock className={Styles.parameterKey}>{parameter.key}</SpanBlock>
+                    {
+                        parameter.value
+                            ? <SpanBlock className={Styles.parameterValue}>{parameter.value}</SpanBlock>
+                            : "NO_VALUE"
+                    }
+                    <SpanBlock className={Styles.parameterType}>{parameter.type}</SpanBlock>
+                </Block>
+            </div>
+            
+            <DeviceParameterModal
+                buttonText={'EDIT'}
+                parameterID={parameter.id}
+                onUpdate={() => {
+                    const currentDeviceID = this.props.match.params.id;
+                    this.props.fetchSmartPlug(currentDeviceID);
+                }}
+            />
+            <Button
+                onClick={() => {
+                    // Hardcoded
+                    // eslint-disable-next-line no-restricted-globals
+                    const result = confirm('Are you sure you want ot delete?')
+                    if(result) {
+                        fetchAPI('DELETE', '/deleteDeviceParameter/'+parameter.id)
+                            .then(async () => {
+                                this.props.fetchSmartPlug(this.state.currentDeviceID);
+                            })
+                            .catch((e) => {
+                                console.error(e)
+                            });
+                    }
+                }}
+            >
+                DEL
+            </Button>
+        </div>
     }
         
     render(){
@@ -114,6 +149,14 @@ class SmartPlugControl extends Component{
                         return this.renderParameter(parameter);
                     }))
                 }
+                <DeviceParameterModal
+                    buttonText={'CREATE'}
+                    mode={"CREATE"}
+                    deviceID={this.state.currentDeviceID}
+                    onCreate={() => {
+                        this.props.fetchSmartPlug(this.state.currentDeviceID);
+                    }}
+                />
                 {this.renderLabeledComponent(
                     "On/Off",
                     (<Switcher onClick={() => this.handleOnOff(!this.state.onOff)} type='rectangular' color={this.state.onOff ? COLORS.neutral: COLORS.disabled} />)
